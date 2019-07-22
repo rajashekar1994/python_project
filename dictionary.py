7229da79bbb4ab71c2abb3172c6499087fa5e4ac
rajashekar1994 <shekar.chennapai@gmail.com>
2019-07-22 17:02:10 +0530

    third commit

---
+++ dictionary.py
@@ -0,0 +1,19 @@
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict["benz"]="a3"
+del thisdict["model"]
+del thisdict["brand"]
+thisdict["model"]="suzuki"
+print("\n",thisdict)
+#del thisdict
+#thisdict.clear()
+#print("\n",thisdict)
+x=thisdict.copy()
+print(len(x))
+y=ascii("rajas@!~ekarraju")
+print(y)
+m=max(10,5)
+print(m)
