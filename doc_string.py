3eab3c694aa3e26b84478b5fb81102b519f2de9a
rajashekar1994 <shekar.chennapai@gmail.com>
2019-07-22 17:28:23 +0530

    22/07/2019

---
+++ doc_string.py
@@ -0,0 +1,16 @@
+class student:
+    '''this is student class developed by rajashekar'''
+    def __init__(self):
+        pass    
+    
+
+
+
+    def list(self):
+        thisdict ={"brand": "Ford","model": "Mustang","year": 1964}
+        print(thisdict)
+    
+print(student.__doc__)
+    
+s=student()
+s.list()
